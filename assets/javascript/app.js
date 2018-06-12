20be0cdd2e8313868751ae57c0f0029407dc3b7d
Frank Santiesteban <franksantiesteban@nancy-vaio.atnind.local>
2018-06-12 14:32:23 -0400

    .append wrapper

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,62 +1,59 @@
-$(document).ready(function() {
-
-$("#remainingTime").hide();
-$("#Start").on("click", trivia.startGame);
-$(document).on("click", '.option', triviaguessChecker);
-
+$("#Start").on("click",function() {
+  for (var i = 0; i < questions.length;i++){
+      $("#subwrapper").append('<h2>'+ questions[i].questions+'<h2>');
+      for(var j=0; j < questions[i].answers.length;j++){
+          $("#subwrapper").append("<inpit type = 'radio' name='questions-"+ i +" ' value= '"+ questions[i].answers[j]+"'>"+questions[i].answers[j])
+      }
+  }
 })
 
-var trivia = {
-    correct: 0,
-    incorrect: 0,
-    unanswered: 0,
-    currentSet: 0,
-    timer: 0,
-    timerOn: false,
-    timerId: "",
 
-    questions: {
-        q1: "Who plays Rocky Balboa?",
-        q2: "What is Rocky's trainers name?",
-        q3: "What's Rocky's nick name?",
-        q4: "What US city was Rocky from?",
-        q5: "Who was Rocky's first opponent?",
-        q6: "Who was the celebrity Rocky fought for charity in Rocky 3?",
-        q7: "Where did he fight in Rocky 4?",
-        q8: "What is the name of his opponent in Rocky 4?",
-        q9: "Who trains Rocky in Rocky 3?",
-        q10: "What is Rocky's wifes name?"
+var questions =[{
+    questions:"Who plays Rocky Balboa?",
+    answers: ['Arnold Schwarzenegger', 'Sylvester Stallone', 'Mr. T', 'Bruce Willis'],
+    correctAnswer:'Sylvester Stallone', 
+}, {
+    questions:"What is Rocky's trainers name?",
+    answers:['Paulie', 'Mickey', 'Johnny', 'Phil'],
+    correctAnswer:'Mickey',
+
+}, {
+    questions:"What's Rocky's nick name?",
+    answers:['The Killer', 'The best in the west', 'The Italian Stalion', 'The Italin'],
+    correctAnswer:'The Italian Stalion',
+}, {
+    questions:"What US city was Rocky from?",
+    answers:['New York', 'Boston', 'Philadelphia', 'New Jersey'],
+    correctAnswer:'Philadelphia',
+}, {
+    questions:"Who was Rocky's first opponent?",
+    answers:['Mr T', 'Hulk Hogan', 'Apollo Creed', 'Mike the Punisher'],
+    correctAnswer:'Apollo Creed',
+}, {
+    questions:"Who was the celebrity Rocky fought for charity in Rocky 3?",
+    answers:['Ravishing Rick', 'The Hulk', 'Thunderlips', 'Clubber Lang'],
+    correctAnswer:'Thunderlips',
+}, {
+    questions:"Where did he fight in Rocky 4?",
+    answers:['Gym', 'Russia', 'New York', 'Alaska'],
+    correctAnswer:'Russia',
+}, {
+    questions: "What is the name of his opponent in Rocky 4?",
+    answers:['Apollo', 'Clubber Lang', 'Ivan Drago', 'Mr T'],
+    correctAnswer:'Ivan Drago',
+}, {
+    questions:"Who trains Rocky in Rocky 3?",
+    answers:['Mickey', 'Paulie', 'Apollo Creed', 'Larry'],
+    correctAnswer:'Apollo Creed',
+}, {
+    questions:"What is Rocky's wifes name?",
+    answers:['Ana', 'Adrian', 'Anabele', 'Amanada'],
+    correctAnswer:'Adrian',
+
+}];
 
 
-    },
-    options: {
-        q1: ['Arnold Schwarzenegger', 'Sylvester Stallone', 'Mr. T', 'Bruce Willis'],
-        q2: ['Paulie', 'Mickey', 'Johnny', 'Phil'],
-        q3: ['The Killer', 'The best in the west', 'The Italian Stalion', 'The Italin'],
-        q4: ['New York', 'Boston', 'Philadelphia', 'New Jersey'],
-        q5: ['Mr T', 'Hulk Hogan', 'Apollo Creed', 'Mike the Punisher'],
-        q6: ['Ravishing Rick', 'The Hulk', 'Thunderlips', 'Clubber Lang'],
-        q7: ['Gym', 'Russia', 'New York', 'Alaska'],
-        q8: ['Apollo', 'Clubber Lang', 'Ivan Drago', 'Mr T'],
-        q9: ['Mickey', 'Paulie', 'Apollo Creed', 'Larry'],
-        q10: ['Ana', 'Adrian', 'Anabele', 'Amanada']
-    },
-    answers: {
-        q1: 'Sylvester Stallone', 
-        q2: 'Mickey',
-        q3: 'The Italian Stalion',
-        q4: 'Philadelphia',
-        q5: 'Apollo Creed',
-        q6: 'Thunderlips',
-        q7: 'Russia',
-        q8: 'Ivan Drago',
-        q9: 'Apollo Creed',
-        q10: 'Adrian',
-        
-    },
 
-    startGame: function(){
-        
-    }
+
 
 
